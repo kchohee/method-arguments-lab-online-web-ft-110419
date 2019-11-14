14478fdf750cb3e40985cde64261aff473dc6a57
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-14 00:12:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+<<<<<<< HEAD
 def introduction_with_language(name, language)
+=======
+def introduction_with_lamguage(name, language)
+>>>>>>> 858ed82d251bf961a27d4f6d0645af9c33058e73
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
